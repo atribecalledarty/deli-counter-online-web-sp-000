17737a06613cb72d6a9aac426c64ab25c262fb1e
Arthur Song <arth3rs0ng@gmail.com>
2019-04-04 01:21:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+    
+  else 
+    katz_deli.each_with_index do |person, index|
+      "#{person}, you are #{index + 1} in line."
+    end
+  end
+end
